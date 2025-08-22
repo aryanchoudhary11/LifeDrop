448e419fb3572690894e28c1d3ac58f854e61624
Aryan <119430003+aryanchoudhary11@users.noreply.github.com>
2025-08-22 23:00:59 +0530

    register controller done with accessToken

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,7 +1,36 @@
-import { validatePassword } from "../utils/validator.js";
-import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import crypto from "crypto";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+import RefreshToken from "../models/RefreshToken.js";
 
+const generateAccessToken = async (user) => {
+  return jwt.sign(
+    { userId: user._id, role: user.role },
+    process.env.JWT_SECRET,
+    { expiresIn: "15m" }
+  );
+};
+
+const generateRefreshToken = async (userId, userAgent, ip) => {
+  const refreshToken = crypto.randomBytes(64).toString("hex");
+  const tokenHash = crypto
+    .createHash("sha256")
+    .update(refreshToken)
+    .digest("hex");
+
+  const expiresAt = new Date();
+  expiresAt.setDate(expiresAt.getDate() + 7);
+
+  await RefreshToken.create({
+    userId,
+    tokenHash,
+    userAgent,
+    ip,
+    expiresAt,
+  });
+  return refreshToken;
+};
 export const register = async (req, res) => {
   try {
     const { name, email, password, role } = req.body;
@@ -10,21 +39,43 @@ export const register = async (req, res) => {
       return res.status(400).json({ message: "Invalid role" });
     }
 
-    if (!validatePassword(password)) {
-      return res.status(400).json({ message: "Week password" });
-    }
-
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(409).json({ message: "Email already exsted" });
     }
 
-    const passwordHash = await bcrypt.hash(password, 10);
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
     const user = await User.create({
       name,
       email,
       passwordHash,
       role,
     });
-  } catch (err) {}
+
+    const accessToken = generateAccessToken(user);
+
+    const refreshToken = await generateRefreshToken(
+      user._id,
+      req.headers["user-agent"] || "unknown",
+      req.ip
+    );
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+      tokens: {
+        accessToken,
+        refreshToken,
+      },
+    });
+  } catch (err) {
+    console.error("Register error:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
--- Backend/utils/validator.js
+++
@@ -1,4 +0,0 @@
-export const validatePassword = (password) => {
-  const strongRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
-  return strongRegex.test(password);
-};
