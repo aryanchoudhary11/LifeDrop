fb1b5a3c012dc3edfb9ceb643e0364e768526a0f
Aryan <119430003+aryanchoudhary11@users.noreply.github.com>
2025-09-05 13:24:00 +0530

    Added listing logic

--- Frontend/src/pages/Donors.jsx
+++ Frontend/src/pages/Donors.jsx
@@ -1,20 +1,52 @@
+import { useEffect } from "react";
 import { useState } from "react";
+import API from "../services/AuthService.js";
 
 export default function Donors() {
   const [donors, setDonors] = useState([]);
   const [pincode, setPincode] = useState("");
   const [bloodType, setBloodType] = useState("");
   const [loading, setLoading] = useState(true);
+
+  const fetchDonors = async (pin = "", blood = "") => {
+    try {
+      setLoading(true);
+      let query = [];
+      if (pin) query.push(`pincode=${pin}`);
+      if (blood) query.push(`bloodType=${blood}`);
+      const { data } = await API.get(
+        query.length > 0 ? `/donors/all?${query.join("&")}` : "/donors/all"
+      );
+      setDonors(data);
+    } catch (err) {
+      console.error("Failed to fetch donors", err);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchDonors();
+  }, []);
+  const handleSearch = (e) => {
+    e.preventDefault();
+    fetchDonors(pincode, bloodType);
+  };
   return (
-    <div>
-      <div>
-        <h1>🩸 Donor Directory</h1>
-        <form>
+    <div className="min-h-screen bg-gradient-to-br from-rose-50 to-red-100 p-6">
+      <div className="max-w-6xl mx-auto">
+        <h1 className="text-4xl font-bold text-center text-rose-600 mb-6 font-['Vend_Sans']">
+          🩸 Donor Directory
+        </h1>
+        <form
+          className="flex flex-col md:flex-row justify-center gap-3 mb-8"
+          onSubmit={handleSearch}
+        >
           <input
             type="text"
             placeholder="Enter Pincode.."
             value={pincode}
             onChange={(e) => setPincode(e.target.value)}
+            className="p-3 border rounded-xl w-60"
           />
           <select
             value={bloodType}
@@ -31,7 +63,12 @@ export default function Donors() {
             <option value="O+">O+</option>
             <option value="O-">O-</option>
           </select>
-          <button type="submit">Search</button>
+          <button
+            type="submit"
+            className="px-6 py-3 bg-rose-600 text-white rounded-xl hover:bg-rose-700 cursor-pointer transition"
+          >
+            Search
+          </button>
         </form>
       </div>
     </div>
