538ef09d2de65956fdbdc6d25bdabcdefc718c84
Aryan <119430003+aryanchoudhary11@users.noreply.github.com>
2025-09-06 17:45:40 +0530

    Mobile menu added for navbar

--- Frontend/src/components/Nabar.jsx
+++ Frontend/src/components/Nabar.jsx
@@ -1,6 +1,7 @@
 import { useNavigate, Link } from "react-router";
 import { useAuth } from "../context/authContext";
 import { useState } from "react";
+import { Menu, X } from "lucide-react";
 
 export default function Navbar() {
   const { auth, setAuth } = useAuth();
@@ -70,7 +71,62 @@ export default function Navbar() {
             </>
           )}
         </div>
+
+        <button
+          className="md:hidden text-gray-700"
+          onClick={() => setOpen(!open)}
+        >
+          {open ? <X size={28} /> : <Menu size={28} />}
+        </button>
       </div>
+
+      {open && (
+        <div className="md:hidden bg-white shadow-lg px-4 py-3 space-y-3 ">
+          {links.map((link) => (
+            <Link
+              key={link.to}
+              to={link.to}
+              onClick={() => setOpen(false)}
+              className="block text-gray-700 hover:text-rose-600 transition"
+            >
+              {link.label}
+            </Link>
+          ))}
+
+          {auth ? (
+            <>
+              <Link
+                to="/profile"
+                onClick={() => setOpen(false)}
+                className="block text-gray-700 hover:text-rose-600 transition"
+              >
+                Profile
+              </Link>
+              <button
+                onClick={handleLogout}
+                className=" text-left px-3 py-1 bg-rose-600 text-white rounded-lg hover:bg-rose-700 transition"
+              >
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <Link
+                to="/login"
+                className="bg-indigo-600 text-white rounded-lg px-3 py-1 hover:bg-indigo-700 cursor-pointer transition"
+              >
+                Login
+              </Link>
+              <Link
+                to="/register"
+                className="bg-rose-600 text-white rounded-lg px-3 py-1 hover:bg-rose-700 cursor-pointer transition"
+              >
+                Register
+              </Link>
+            </>
+          )}
+        </div>
+      )}
     </nav>
   );
 }
